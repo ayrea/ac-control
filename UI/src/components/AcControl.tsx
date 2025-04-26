6357091209e2111450542efbf11dc8ce2f17faa8
aayre <>
2025-04-26 21:07:33 +0800

    Update UI to fix sizing for iPhone

--- UI/src/components/AcControl.tsx
+++ UI/src/components/AcControl.tsx
@@ -1,6 +1,6 @@
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-import { Button, Grid, MenuItem, Select, SelectChangeEvent, SvgIcon, Switch, SxProps, Typography } from '@mui/material';
+import { Button, Grid, MenuItem, Select, SelectChangeEvent, SvgIcon, Switch, Typography } from '@mui/material';
 import { ReactElement, useState } from 'react';
 import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
@@ -109,13 +109,6 @@ const acInitialState = (): AcState => {
     }
 }
 
-const commonStyles: SxProps = {
-    bgcolor: 'background.paper',
-    borderColor: 'text.primary',
-    border: 1,
-    borderRadius: '16px'
-};
-
 export default function AcControl() {
     const [acState, setAcState] = useState<AcState>(acInitialState);
 
@@ -143,7 +136,7 @@ export default function AcControl() {
 
     const handleChangeMode = (e: SelectChangeEvent) => {
         const selectedMode: AcMode = acModes.find((mode) => mode.index.toString() === e.target.value.toString()) ?? acModes[0];
-        setAcState({ ...acState, mode: {...selectedMode} });
+        setAcState({ ...acState, mode: { ...selectedMode } });
     }
 
     const handleChangeFanSpeed = (e: SelectChangeEvent) => {
@@ -161,70 +154,58 @@ export default function AcControl() {
     }
 
     return (
-        <Paper elevation={6} sx={{ maxWidth: "550px" }}>
+        <Paper elevation={6} sx={{ maxWidth: "560px" }}>
+            <Box height={"4px"}>
+            </Box>
+            <Box m={1} pl={2} bgcolor={"lightgrey"} justifyItems={'left'} borderRadius={3} sx={{ bgcolor: "#d7e4ef" }}>
+                <Typography variant='h4'>AC Control</Typography>
+            </Box>
             <Grid container spacing={1}>
-                <Grid size={12} m={1} pl={2} bgcolor={"lightgrey"} justifyItems={'left'} borderRadius={3} sx={{ bgcolor: "#d7e4ef" }}>
-                    <Typography variant='h4'>AC Control</Typography>
+                <Grid size={4} justifyItems={'right'} />
+                <Grid size={8} justifyItems={'left'} sx={{ mb: 2 }}>
+                    <Box m={1} mt={1} display={"flex"}>
+                        <Button variant='contained' sx={acState.onOff ? { bgcolor: "#00c200" } : { bgcolor: "grey" }} onClick={togglePower}><PowerSettingsNewIcon fontSize='large' /></Button>
+                        <Typography variant='h4' mx={4} mt={0.5}>{acState.onOff ? "On" : "Off"}</Typography>
+                    </Box>
                 </Grid>
-
-                <Grid size={4} justifyItems={'right'}>
-                    <Typography variant='h6' mt={2}>Power:</Typography>
-                </Grid>
-
-                <Box display={"flex"} sx={{ ...commonStyles, width: "250px" }}>
-                    <Grid size={8} justifyItems={'left'}>
-                        <Box display={"flex"}>
-                            <Box m={1} display={"flex"}>
-                                <Button variant='contained' sx={acState.onOff ? { bgcolor: "#00c200" } : { bgcolor: "grey" }} onClick={togglePower}><PowerSettingsNewIcon fontSize='large' /></Button>
-                                <Typography variant='h4' mx={7.5} mt={0.5}>{acState.onOff ? "On" : "Off"}</Typography>
-                            </Box>
-                        </Box>
-                    </Grid>
-                </Box>
-
-                <Grid size={5} my={2} justifyItems={'right'}>
+                <Grid size={5} justifyItems={'right'}>
                     <Typography variant='h6' mt={"4px"}>Current Temp:</Typography>
                 </Grid>
-                <Grid size={7} my={2} justifyItems={'left'}>
+                <Grid size={7} mb={1} justifyItems={'left'}>
                     <Typography ml={1} variant='h4'>{acState.currentTemp.toFixed(1)}</Typography>
                 </Grid>
 
                 <Grid size={4} justifyItems={'right'}>
                     <Typography variant='h6' mt={2}>Set Temp:</Typography>
                 </Grid>
-                <Grid size={8} justifyItems={'left'} sx={{ mb: 2 }}>
-                    <Box display={"flex"} sx={{ ...commonStyles, width: "250px" }}>
-                        <Box m={1} mt={1.5} display={"flex"}>
-                            <Button id="tempDown" variant='contained' sx={{ width: "50px", height: "36px", bgcolor: "lightgray" }} onClick={temperatureUpDown}>
-                                <SvgIcon sx={{ width: "50px", height: "50px", color: "black" }}><ArrowDropDownIcon fontSize='large' /></SvgIcon>
-                            </Button>
-                            <Typography variant='h4' mx={2}>{acState.setTemp.toFixed(1)}</Typography>
-                            <Button id="tempUp" variant='contained' sx={{ width: "50px", height: "36px", bgcolor: "lightgray" }} onClick={temperatureUpDown}>
-                                <SvgIcon sx={{ width: "50px", height: "50px", color: "black" }}><ArrowDropUpIcon fontSize='large' /></SvgIcon>
-                            </Button>
-                        </Box>
+                <Grid size={8} justifyItems={'left'} sx={{ mb: 1 }}>
+                    <Box m={1} mt={1.5} display={"flex"}>
+                        <Button id="tempDown" variant='contained' sx={{ width: "50px", height: "36px", bgcolor: "lightgray" }} onClick={temperatureUpDown}>
+                            <SvgIcon sx={{ width: "50px", height: "50px", color: "black" }}><ArrowDropDownIcon fontSize='large' /></SvgIcon>
+                        </Button>
+                        <Typography variant='h4' mx={2}>{acState.setTemp.toFixed(1)}</Typography>
+                        <Button id="tempUp" variant='contained' sx={{ width: "50px", height: "36px", bgcolor: "lightgray" }} onClick={temperatureUpDown}>
+                            <SvgIcon sx={{ width: "50px", height: "50px", color: "black" }}><ArrowDropUpIcon fontSize='large' /></SvgIcon>
+                        </Button>
                     </Box>
                 </Grid>
-
                 <Grid size={4} justifyItems={'right'}>
                     <Typography variant='h6' mt={2.5}>Mode:</Typography>
                 </Grid>
-                <Grid size={8} justifyItems={'left'} sx={{ mb: 2 }}>
-                    <Box display={"flex"} sx={{ ...commonStyles, width: "250px" }}>
-                        <Box m={1} mt={1.5} display={"flex"}>
-                            <Select sx={{ width: "230px", height: '46px' }} value={acState.mode.index.toString()} onChange={handleChangeMode}>
-                                {acModes.map((mode) => {
-                                    return (
-                                        <MenuItem key={mode.label} value={mode.index}>
-                                            <Box display={"flex"}>
-                                                {mode.icon}
-                                                <Typography ml={2} variant='h4'>{mode.label}</Typography>
-                                            </Box>
-                                        </MenuItem>
-                                    );
-                                })};
-                            </Select>
-                        </Box>
+                <Grid size={8} justifyItems={'left'} sx={{ mb: 1 }}>
+                    <Box m={1} mt={1.5} display={"flex"}>
+                        <Select sx={{ width: "230px", height: '46px' }} value={acState.mode.index.toString()} onChange={handleChangeMode}>
+                            {acModes.map((mode) => {
+                                return (
+                                    <MenuItem key={mode.label} value={mode.index}>
+                                        <Box display={"flex"}>
+                                            {mode.icon}
+                                            <Typography ml={2} variant='h4'>{mode.label}</Typography>
+                                        </Box>
+                                    </MenuItem>
+                                );
+                            })};
+                        </Select>
                     </Box>
                 </Grid>
 
@@ -232,16 +213,14 @@ export default function AcControl() {
                     <Typography variant='h6' mt={2.5}>Fan Speed:</Typography>
                 </Grid>
                 <Grid size={8} justifyItems={'left'} sx={{ mb: 2 }}>
-                    <Box display={"flex"} sx={{ ...commonStyles, width: "250px" }}>
-                        <Box m={1} mt={1.5} display={"flex"}>
-                            <Select sx={{ width: "230px", height: '46px' }} value={acState.fanSpeed.toString()} onChange={handleChangeFanSpeed}>
-                                {getEnumKeys(FanSpeedEnum).map((fanSpeed: string, index: number) => {
-                                    return (<MenuItem key={index} value={index}>
-                                        <Typography variant='h4'>{fanSpeed}</Typography>
-                                    </MenuItem>);
-                                })}
-                            </Select>
-                        </Box>
+                    <Box m={1} mt={1.5} display={"flex"}>
+                        <Select sx={{ width: "230px", height: '46px' }} value={acState.fanSpeed.toString()} onChange={handleChangeFanSpeed}>
+                            {getEnumKeys(FanSpeedEnum).map((fanSpeed: string, index: number) => {
+                                return (<MenuItem key={index} value={index}>
+                                    <Typography variant='h4'>{fanSpeed}</Typography>
+                                </MenuItem>);
+                            })}
+                        </Select>
                     </Box>
                 </Grid>
 
